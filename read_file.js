30e00046e4e866c01cddf6d071eb119fbc8bf3b0
Marie-AnneS <marie-anne.seim@gmail.com>
2019-05-06 17:12:33 +0000

    ajout des fichier

---
+++ read_file.js
@@ -0,0 +1,12 @@
+var input = process.argv[2];
+var fs = require("fs");
+
+var filePath = "/tmp/test_async.txt";
+var fileData = "Testing asynchronous file write.\n";
+
+fs.writeFile(filePath, fileData, function(err) {
+  if (err) {
+    throw err;
+  }
+  console.log("Successfully wrote to", filePath);
+});
